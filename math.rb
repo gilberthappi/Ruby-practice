694021079616dda242caaf5ebb96cdde2185cd2a
gilberthappi <gdushimimana6@gmail.com>
2024-03-23 10:47:08 +0200

    Add Math in ruby

---
+++ math.rb
@@ -0,0 +1,16 @@
+# puts -5.4578
+# puts 5 + 9
+# puts 3**3
+# puts 10%3
+
+num = -2.80
+
+# puts ("my fav number " +num.to_s) //to change to string .to_s
+# puts num.abs()// absolute value
+# puts num.round()
+# puts num.ceil()
+
+# puts Math.sqrt(64)
+# puts Math.log(1)
+
+# puts 10/7.0
