87855efa8036ffbd32df26c09425ebd73fce83a7
gilberthappi <gdushimimana6@gmail.com>
2024-03-23 10:35:42 +0200

    Add strings methods in Rudy

---
+++ strings.rb
@@ -0,0 +1,13 @@
+puts "Hello\"\n world"
+
+phrase = "Ruby Coding"
+# puts phrase.upcase()//upper leeter
+# puts phrase.downcase()//small letter
+# puts phrase.strip() //remove space around string
+# puts phrase.length()//length of word 
+# puts phrase.include? "Coding"//checking 
+# puts phrase[0]//access first letter using index
+# puts phrase[7].upcase()
+# puts phrase[0,3]//access first 3 character
+# puts phrase.index("d")//finding index of character
+puts "programming".upcase
